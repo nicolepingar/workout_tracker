971e67eca9180cc1e23eeccbb52c393802f8db94
Nicole Pingar <nicolepingar@yahoo.com>
2021-11-01 18:15:12 -0400

    commented out server.js lines 21-23

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,9 +3,9 @@ const db = require('../models');
 
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+  // useUnifiedTopology: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false
 });
 
 const workoutSeed = [
