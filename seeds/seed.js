410a42e344d7a68fa9ea81b4bf67408878b221f7
Nicole Pingar <nicolepingar@yahoo.com>
2021-11-02 09:47:26 -0400

    deleted stuff not needed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect('mongodb://localhost/workout', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect('mongodb://localhost/workout');
 
 const workoutSeed = [
   {
--- server.js
+++ server.js
@@ -15,13 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout");
 
 // routes
 app.use(require('./routes/api'));
